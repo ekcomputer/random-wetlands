68c7d545f0894958db5ba6915cd6bf5b2ca643a9
ethankyzivat <ethan_kyzivat@brown.edu>
2021-01-15 13:07:41 -0500

    checkpoint. Changed reclass for run40, added post-class filtering.

--- analysis/python_env.py
+++ analysis/python_env.py
@@ -1,20 +1,21 @@
 ''' Environment variables used for python analysis scripts'''
 import os
 
-# Classes: Run 38,29        orig    reclass_old reclass
-#                 1       W1 W      10          2
-#                 2       SW WE     9           3
-#                 3       HW W      10          2
-#                 4       BA W      10          2
-#                 5       GW WE     9           3
-#                 6       GD        4           1
-#                 7       SD        3           1
-#                 8       FD        1           1
-#                 9       FD2       1           1
-#                 10      WD        3           1
-#                 11      W2 W      10          2
-#                 12      BG        5           1
-#                 13      FW WE     9           3
+# Classes: Run 38,39,40     orig    reclass_old reclass 2021_reclass
+#                 1       W1 W      10          2       2
+#                 2       SW WE     9           3       4
+#                 3       HW W      10          2       2
+#                 4       BA W      10          2       2
+#                 5       GW WE     9           3       3
+#                 6       GD        4           1       1
+#                 7       SD        3           1       1
+#                 8       FD        1           1       1
+#                 9       FD2       1           1       1
+#                 10      WD        3           1       1
+#                 11      W2 W      10          2       2
+#                 12      BG        5           1       1
+#                 13      FW WE     9           3       5
+
 
 
 ''' 
@@ -32,6 +33,12 @@ classes_re:
     2   Open water
     3   Littoral
 
+classes_re_2: 
+    1   Dry land
+    2   Open water
+    3   Wet graminoid
+    4   Wet shrubs
+    5   Wet forest
 
 '''
     # Constants
@@ -40,16 +47,19 @@ urban_val=30
 NODATAVALUE=0
 
     # Classes: run 39
-# classes={'wet': [1,2,3,4,5,11,13], 'wet_emergent':[2,5,13], 'water': [1,3,4,11]} # not used: water
-# classes_re={0:0, 1:2, 2:3, 3:2, 4:2, 5:3, 6:1, 7:1, 8:1, 9:1, 10:1, 11:2, 12:1, 13:3, urban_val:1}
+classes={'wet': [1,2,3,4,5,11,13], 'wet_emergent':[2,5,13], 'water': [1,3,4,11]} # not used: water
+# classes_re={0:0, 1:2, 2:3, 3:2, 4:2, 5:3, 6:1, 7:1, 8:1, 9:1, 10:1, 11:2, 12:1, 13:3, urban_val:1} # classes_re: for three classes: dry, wet emergent, open water
+classes_re={0:0, 1:2, 2:4, 3:2, 4:2, 5:3, 6:1, 7:1, 8:1, 9:4, 10:1, 11:2, 12:1, 13:3, urban_val:1} # 2021 classes_re_2: for five classes: dry, wet graminoid, wet shrubs, wet forest, open water
+
 
     # Classes: run EngramLakes
-classes={'wet': [1,3,11,2,5,13], 'wet_emergent':[2, 5,13], 'water': [1,3,11]} # not used: water
-classes_re={0:0, 1:2, 2:3,3:2,4:2,5:3,6:1,7:1,8:1,9:1,10:1,11:2,12:1,13:3,14:1, urban_val:1}
+# classes={'wet': [1,3,11,2,5,13], 'wet_emergent':[2, 5,13], 'water': [1,3,11]} # not used: water
+# classes_re={0:0, 1:2, 2:3,3:2,4:2,5:3,6:1,7:1,8:1,9:1,10:1,11:2,12:1,13:3,14:1, urban_val:1}
 
     # paths
-# base_dir='/mnt/f/PAD2019/classification_training/PixelClassifier/Test39/batch2'
-base_dir='/mnt/f/PAD2019/classification_training/PixelClassifier/TestEngramLakes/Test35-Fr-LUT-LUT/inc-clip'
+base_dir='/mnt/f/PAD2019/classification_training/PixelClassifier/Test40'                                 # Classes: run 40
+# base_dir='/mnt/f/PAD2019/classification_training/PixelClassifier/Test39/batch2'                                 # Classes: run 39
+# base_dir='/mnt/f/PAD2019/classification_training/PixelClassifier/TestEngramLakes/Test35-Fr-LUT-LUT/inc-clip'  # Classes: run EngramLakes
 bridges_pth='/mnt/f/PAD2019/classification_training/PixelClassifier/Test38/bridges/bridges.shp'
 
     # Dynamic variables
--- analysis/reclass_rasters.py
+++ analysis/reclass_rasters.py
@@ -1,17 +1,17 @@
-''' Modified from Raster2PercentagePoly.py '''
-import os
+''' Modified from Raster2PercentagePoly.py. Now includes step for post-classification smoothing! '''
+# from analysis.python_env import NODATAVALUE
 import glob
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
 import rasterio as rio
-from rasterio import plot, features
+from rasterio import features, plot
 from rasterio.features import shapes
+from skimage import filters, morphology
 
-from utils import reclassify
-
-## TODO: 
-# Import env variables
 from python_env import *
+from utils import reclassify
 
 ## dynamic IO
 os.makedirs(reclass_dir, exist_ok=True)
@@ -20,7 +20,13 @@ os.makedirs(reclass_dir, exist_ok=True)
 # load using rasterio
 files_in=glob.glob(base_dir + os.sep + '*cls.tif')
 
-for i in range(len(files_in)):
+## print reportin
+print('Class dictionary: ', classes_re)
+
+## loop
+# for i in [0]:                   # toggle to only work on one file
+for i in range(len(files_in)):  # toggle to only work on all files
+
     landcover_in_path=files_in[i] # '/mnt/f/PAD2019/classification_training/PixelClassifier/Test35/padelE_36000_19059_003_190904_L090_CX_01_LUT-Freeman_cls.tif'
     landcover_out_path=landcover_in_path.replace('cls.tif', 'rcls.tif').replace(base_dir, reclass_dir)
     print(f'\n\n----------------\nInput:\t{landcover_in_path}')
@@ -36,12 +42,20 @@ for i in range(len(files_in)):
         with rio.open(landcover_in_path) as src:
             lc = src.read(1)
 
-        # reclassify
+        ## reclassify
         lc_out=reclassify(lc, classes_re)
+        # lc_out=lc # if no reclassify
+
+        ## Classification post-processing (smoothing with majority filter)
+        selem = morphology.disk(3)
+        lc_out= filters.rank.majority(lc_out, selem, mask=lc_out != NODATAVALUE)
+
     # lc_out=lc # temp
 
         # write out
         with rio.open(landcover_out_path, 'w', **profile) as dst:
             dst.write(lc_out, 1)
         print(f'Output written.')
+print('Loop finished.')
+
 # for loading in blocks (and add parallel), see: https://gis.stackexchange.com/questions/368874/read-and-then-write-rasterio-geotiff-file-without-loading-all-data-into-memory
