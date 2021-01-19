f46137308668452f883a204e7a1cdc6f8c49abbb
ethankyzivat <ethan_kyzivat@brown.edu>
2021-01-19 15:23:00 -0500

    Added Daring class for re-classification and auto-detection based on filename.

--- analysis/python_env.py
+++ analysis/python_env.py
@@ -16,6 +16,21 @@ import os
 #                 12      BG        5           1       1
 #                 13      FW WE     9           3       5
 
+# Classes: Run 35 (Freeman-LUT, Daring)         reclass 2021_reclass
+#                 1       W1 W                  2       2
+#                 2       SW WE                 3       4
+#                 3       HW W                  2       2
+#                 4       TW                            3
+#                 5       GW WE                 3       3
+#                 6       GD                    1       1
+#                 7       SD                    1       1
+#                 8       FD                    1       1
+#                 9       FD2                   1       1
+#                 10      TD                            1
+#                 11      W2 W                  2       2
+#                 12      BG                    1       1
+#                 13      FW WE                 3       5
+#                 14      WD                            1
 
 
 ''' 
@@ -51,6 +66,7 @@ SELEM_DIAM=3 # for majortiy filter
 classes={'wet': [1,2,3,4,5,11,13], 'wet_emergent':[2,5,13], 'water': [1,3,4,11]} # not used: water
 # classes_re={0:0, 1:2, 2:3, 3:2, 4:2, 5:3, 6:1, 7:1, 8:1, 9:1, 10:1, 11:2, 12:1, 13:3, urban_val:1} # classes_re: for three classes: dry, wet emergent, open water
 classes_re={0:0, 1:2, 2:4, 3:2, 4:2, 5:3, 6:1, 7:1, 8:1, 9:4, 10:1, 11:2, 12:1, 13:3, urban_val:1} # 2021 classes_re_2: for five classes: dry, wet graminoid, wet shrubs, wet forest, open water
+classes_re_daring={0:0, 1:2, 2:4, 3:2, 4:3, 5:3, 6:1, 7:1, 8:1, 9:4, 10:1, 11:2, 12:1, 13:3, 14:1, urban_val:1} # 2021 classes_re_2 from daring: for five classes: dry, wet graminoid, wet shrubs, wet forest, open water
 
 
     # Classes: run EngramLakes
@@ -58,7 +74,8 @@ classes_re={0:0, 1:2, 2:4, 3:2, 4:2, 5:3, 6:1, 7:1, 8:1, 9:4, 10:1, 11:2, 12:1, 
 # classes_re={0:0, 1:2, 2:3,3:2,4:2,5:3,6:1,7:1,8:1,9:1,10:1,11:2,12:1,13:3,14:1, urban_val:1}
 
     # paths
-base_dir='/mnt/f/PAD2019/classification_training/PixelClassifier/Test40'                                 # Classes: run 40
+# base_dir='/mnt/f/PAD2019/classification_training/PixelClassifier/Test35'                                         # Classes: run 35 (Daring)
+base_dir='/mnt/f/PAD2019/classification_training/PixelClassifier/Test40'                                         # Classes: run 40
 # base_dir='/mnt/f/PAD2019/classification_training/PixelClassifier/Test39/batch2'                                 # Classes: run 39
 # base_dir='/mnt/f/PAD2019/classification_training/PixelClassifier/TestEngramLakes/Test35-Fr-LUT-LUT/inc-clip'  # Classes: run EngramLakes
 bridges_pth='/mnt/f/PAD2019/classification_training/PixelClassifier/Test38/bridges/bridges.shp'
--- analysis/reclass_rasters.py
+++ analysis/reclass_rasters.py
@@ -26,8 +26,8 @@ print('Class dictionary: ', classes_re)
 ## loop
 
 #################################################
-for i in [0]:                     # toggle to only work on one file
-# for i in range(len(files_in)):      # toggle to only work on all files
+# for i in [0]:                     # toggle to only work on one file
+for i in range(len(files_in)):      # toggle to only work on all files
 #################################################
 
     landcover_in_path=files_in[i] # '/mnt/f/PAD2019/classification_training/PixelClassifier/Test35/padelE_36000_19059_003_190904_L090_CX_01_LUT-Freeman_cls.tif'
@@ -49,7 +49,12 @@ for i in [0]:                     # toggle to only work on one file
             lc = src.read(1)
 
         ## reclassify
-        lc_out=reclassify(lc, classes_re)
+        if 'daring' in landcover_in_path:
+            print('Using different class dictionary for Daring scenes.')
+            print('Class dictionary: ', classes_re_daring)
+            lc_out=reclassify(lc, classes_re_daring)    # use a different class dictionary
+        else:   
+            lc_out=reclassify(lc, classes_re)           # otherwise, proceed normally
         # lc_out=lc # if no reclassify
 
         ## Classification post-processing (smoothing with majority filter)
